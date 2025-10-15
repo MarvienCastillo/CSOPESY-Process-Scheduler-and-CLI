78a9f565bac20be6365b2b42b812d5fdcd67998b
Marvien Castillo <160254067+MarvienCastillo@users.noreply.github.com>
2025-10-15 21:36:55 +0800

    a

--- main.cpp
+++ main.cpp
@@ -11,6 +11,15 @@ std::string ascii_art = R"(
 )";
 std::string header = "Welcome to CSOPESY Emulator!\n\nGroup Developers:\nCastillo, Marvien Angel\nHerrera, Mikaela Gabrielle\nJimenez, Jaztin Jacob\nRegindin, Sean Adrien\n\nLast Updated: 10-15-2025\n";
 int main(){
+    std::string command = "";
     std::cout << ascii_art << header << "\n--------------------------------------\n" << std::flush;
+    std::cout << "\n\nroot:\\>";
+    std::cin >> command;
+    if(command == "initialize"){
+        std::cout << "Initializing";
+    }
+    else{
+        exit(0);
+    }
     return 0;
 }
