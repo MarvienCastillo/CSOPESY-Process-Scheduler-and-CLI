1608dd8cb94c9c62f36d189567ed1a752313cdfb
Marvien Castillo <160254067+MarvienCastillo@users.noreply.github.com>
2025-10-28 21:58:35 +0800

    a

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-bool is_running = false;
+#include <fstream>
+#include <string>
 std::string ascii_art = R"(
   ____ ____   ____  _____  _______ _________   __
  / ___/ ___| / _  \|  _  \|  _____/  ____\  \ / /
@@ -10,13 +11,43 @@ std::string ascii_art = R"(
 --------------------------------------------------
 )";
 std::string header = "Welcome to CSOPESY Emulator!\n\nGroup Developers:\nCastillo, Marvien Angel\nHerrera, Mikaela Gabrielle\nJimenez, Jaztin Jacob\nRegindin, Sean Adrien\n\nLast Updated: 10-15-2025\n";
+
+#define MAX_PROCESS 120
+
+
+typedef struct {
+    int numCPU;
+    std:: string schedulingAlgorithm;
+    int timeQuantum;
+    int batchFreq;
+    int minCommand;
+    int maxCommand;
+    int delayTime;
+} Config;
+
+class Process{
+public:
+    std::string name;
+    int coreAssigned;
+    int currentInstruction;
+    bool isFinished;
+    time_t startTime;
+    time_t endTime;
+
+    Process()
+
+};
+
+class Screen {
+
+};
 int main(){
     std::string command = "";
     std::cout << ascii_art << header << "\n--------------------------------------\n" << std::flush;
     std::cout << "\n\nroot:\\>";
     std::cin >> command;
     if(command == "initialize"){
-        std::cout << "Initializing";
+        
     }
     else{
         exit(0);
